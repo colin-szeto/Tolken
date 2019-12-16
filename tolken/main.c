c72ef55627f5d7d37e047866c570d88837b47bae
kolin <kolinszeto@gmail.com>
2019-12-16 14:25:25 -0800

    Initial Commit

---
+++ tolken/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  tolken
+//
+//  Created by Szeto, Colin on 12/16/19.
+//  Copyright © 2019 Szeto, Colin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
